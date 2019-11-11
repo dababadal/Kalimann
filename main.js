3bcfde44c0fe10dbd6bc2b7e1cafb67672427195
dababadal <mohammedsalimboulaajine@gmail.com>
2019-11-11 18:30:33 +0100

    all

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const { app, BrowserWindow } = require('electron')
  
 function createWindow () {
   let win = new BrowserWindow({ width: 800, height: 600, titleBarStyle: 'hidden' })
-  win.show()
   win.loadFile('index.html')
 }
  
