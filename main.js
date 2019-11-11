1554873bfeef79bc2a7feb5972234a48bf595daa
dababadal <mohammedsalimboulaajine@gmail.com>
2019-11-11 18:25:49 +0100

    test

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const { app, BrowserWindow } = require('electron')
  
 function createWindow () {
-  let win = new BrowserWindow({ width: 800, height: 600 });
+  let win = new BrowserWindow({ width: 800, height: 600 })
   win.setMenu(null);
-  win.loadFile('index.html');
+  win.loadFile('index.html')
 }
  
 app.on('ready', createWindow)
